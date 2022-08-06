47b6a273b8ac194cd83fa34c58520fe69ff0403d
Mertcan <mert_06_can@outlook.com>
2022-08-06 22:31:16 +0300

    Add project files.

---
+++ Crudpostgresql/Model/ApiRestonce.cs
@@ -0,0 +1,15 @@
+﻿namespace Crudpostgresql.Model
+{
+    public class ApiRestonce
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public object? ResponseData { get; set; }
+    }
+    public enum ResponseType
+    {
+        Success,
+        NotFound,
+        Failure
+    }
+}
---
+++ Crudpostgresql/Model/ResponseHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace Crudpostgresql.Model
+{
+    public class ResponseHandler
+    {
+        public static ApiRestonce GetExceptionResponse(Exception ex)
+        {
+            ApiRestonce response = new ApiRestonce();
+            response.Code = "1";
+            response.ResponseData = ex.Message;
+            return response;
+        }
+        public static ApiRestonce GetAppResponse(ResponseType type, object? contract)
+        {
+            ApiRestonce response;
+
+            response = new ApiRestonce { ResponseData = contract };
+            switch (type)
+            {
+                case ResponseType.Success:
+                    response.Code = "0";
+                    response.Message = "Success";
+
+                    break;
+                case ResponseType.NotFound:
+                    response.Code = "2";
+                    response.Message = "No record available";
+                    break;
+            }
+            return response;
+        }
+    }
+}
